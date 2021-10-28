7f95242657912e568977e15369e6222955bdc27e
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-28 19:27:55 +0700

    feat(widget): add custom widget NormalText

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Home(),
-    )
-  );
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+TextStyle normalTextStyle = const TextStyle(
+  fontFamily: 'ZenKakuGothicAntique',
+  fontWeight: FontWeight.w400
+);
+
+class NormalText extends StatelessWidget {
+  final String data;
+
+  const NormalText(this.data, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(data, style: normalTextStyle);
+  }
 }
 
 class Home extends StatelessWidget {
@@ -15,7 +29,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Hello world !"),
+        child: NormalText("Hello world !")
       )
     );
   }
